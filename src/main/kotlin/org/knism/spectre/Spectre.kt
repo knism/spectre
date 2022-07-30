48ca5ac79a7c8b5cf52600d1c3399fea72acc59a
pavel <pavel.sedlacek2004@gmail.com>
2022-07-30 10:02:32 +0200

    Spectre.kt provides applicationRef and ApplicationDI

--- src/main/kotlin/org/knism/spectre/Spectre.kt
+++ src/main/kotlin/org/knism/spectre/Spectre.kt
@@ -1,8 +1,12 @@
 package org.knism.spectre
 
+import org.knism.spectre.app.App
+import org.knism.spectre.di.ApplicationDI
 import org.knism.spectre.window.Window
 
+/** Core static object for holding app state independent data. */
 object Spectre {
+    val di: ApplicationDI = ApplicationDI()
     lateinit var window: Window
-    lateinit var
+    lateinit var app: App
 }
